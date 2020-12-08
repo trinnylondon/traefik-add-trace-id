9570ffa2a854fa6d91c864def31fc46f0bd1c9cf
Dionisio Perez-Mavrogenis <dio@trinnylondon.com>
2020-12-08 10:34:54 +0000

    adding options for verbose

--- trace-id.go
+++ trace-id.go
@@ -3,6 +3,7 @@ package traefik_add_trace_id
 import (
 	"context"
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -12,6 +13,7 @@ const defaultTraceId = "X-Trace-Id"
 type Config struct {
 	HeaderPrefix string `json:"headerPrefix"`
 	HeaderName   string `json:"headerName"`
+	Verbose      bool   `json:"verbose"`
 }
 
 // CreateConfig creates the default plugin configuration.
@@ -28,13 +30,15 @@ type TraceIDHeader struct {
 	headerPrefix string
 	name         string
 	next         http.Handler
+	verbose      bool
 }
 
 // New created a new TraceIDHeader plugin.
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
 	tIDHdr := &TraceIDHeader{
-		next: next,
-		name: name,
+		next:    next,
+		name:    name,
+		verbose: config.Verbose,
 	}
 
 	if config == nil {
@@ -62,6 +66,10 @@ func (t *TraceIDHeader) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 		req.Header.Add(t.headerName, randomUUID)
 	} else if headerArr[0] == "" {
 		req.Header[t.headerName][0] = randomUUID
+	}
+
+	if t.verbose {
+		log.Println(req.Header[t.headerName][0])
 	}
 
 	t.next.ServeHTTP(rw, req)
